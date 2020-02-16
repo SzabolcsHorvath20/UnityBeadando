9a6bc04187fc9b3965f9c6a4917073d84a3c68ba
SzabolcsHorvath20 <47601172+SzabolcsHorvath20@users.noreply.github.com>
2020-02-16 23:36:43 +0100

    Add files via upload

---
+++ Assets/Random.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random : MonoBehaviour
+{
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
+    void Start()
+    {
+        x = UnityEngine.Random.Range(-17, 18);
+        y = UnityEngine.Random.Range(-17, 18);
+        z = 0;
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+    }
+}
---
+++ Assets/RandomAsteroid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAsteroid : MonoBehaviour
+{
+
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
+    public int amount;
+    public int bordernegative;
+    public int borderpoitive;
+    public GameObject gameObject;
+    void Start()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            x = UnityEngine.Random.Range(bordernegative, borderpoitive);
+            y = UnityEngine.Random.Range(bordernegative, borderpoitive);
+            z = 0;
+            pos = new Vector3(x, y, z);
+            Instantiate(gameObject, pos, Quaternion.identity);
+        }
+    }
+
+}
